23e94d1358175efda789b9a9bdf3b23a428a35b2
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2022-01-07 17:13:26 -0500

    improving code readability

--- components/assets/BackButton.js
+++ components/assets/BackButton.js
@@ -3,21 +3,26 @@ import { View, TouchableOpacity, Platform } from 'react-native';
 import {withNavigation} from 'react-navigation';
 // import Icon from 'react-native-vector-icons/Ionicons';
  
- class BackButton extends React.Component {
+ class BackButton extends React.Component 
+ {
  
-  render() {
+  render() 
+  {
         return (
             <TouchableOpacity
-            onPress={() => this.props.navigation.goBack()}>
-            <View style={{  padding:20,justifyContent: 'center', alignItems: 'center' }}>
-                {/* Back Icon */}
-            {/* <Icon name="md-arrow-back" size={25} color="#000000" /> */}
-            <Text>Back</Text>
-            </View>
+                onPress={() => this.props.navigation.goBack()}>
+
+              <View style={{  padding:20,justifyContent: 'center', alignItems: 'center' }}>
+                  {/* Back Icon */}
+                  {/* <Icon name="md-arrow-back" size={25} color="#000000" /> */}
+                  <Text>Back</Text>
+              </View>
+
             </TouchableOpacity>
           );
       
     
   }
 }
+
 export default withNavigation(BackButton);
